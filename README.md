10a97819390ee8e5b9ea6f0351b4e2d8436c1247
Lee Liu <lee@logdna.com>
2018-07-18 16:00:54 -0700

    initial checkin

---
+++ README.md
@@ -0,0 +1,30 @@
+# LogDNA CloudWatch
+
+The LogDNA AWS CloudWatch integration relies on [AWS Lambda](https://aws.amazon.com/documentation/lambda/) to route your [CloudWatch Logs](http://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/WhatIsCloudWatchLogs.html) to LogDNA.
+
+## Configure the LogDNA AWS Lambda function
+
+1. Create a new Lambda function and skip to the Configure function section
+2. Set the following fields:
+* Name: `LogDNA`
+* Runtime: `Python 2.7`
+* Code entry type: `Upload a .ZIP file`
+* Upload a [.ZIP file](https://github.com/answerbook/lambda/raw/master/bin/lambda.zip)
+* Handler: `logdna_cloudwatch.lambda_handler`
+* Environment variables: 
+    * (Required) LOGDNA_KEY: LOGDNA_KEY YOUR_INGESTION_KEY_HERE
+    * (Optional) LOGDNA_HOSTNAME: Alternative Host Name
+    * (Optional) LOGDNA_TAGS: Comma-separated Tags
+3. Assign an IAM user with basic execution permissions
+4. Click `Create function`
+
+## Configure your AWS CloudWatch Stream
+
+1. Select the CloudWatch Log Group to upload to LogDNA
+2. Click the Actions menu and select `Stream to AWS Lambda`
+3. Select the `LogDNA` Lambda function and click `Next`
+4. Select the desired log format and click `Start Streaming`
+
+## Contributing
+
+Contributions are always welcome. See the [contributing guide](/CONTRIBUTING.md) to learn how you can help. Build instructions for the agent are also in the guide.
---
+++ logdna_cloudwatch.py
@@ -0,0 +1,64 @@
+from botocore.vendored import requests
+import os
+import json
+import gzip
+from StringIO import StringIO
+
+LOGDNA_URL = 'https://logs.logdna.com/logs/ingest'
+MAX_LINE_LENGTH = 32000
+MAX_REQUEST_TIMEOUT = 30
+
+def lambda_handler(event, context):
+    key, hostname, tags = setup()
+    cw_log_lines = decodeEvent(event)
+    messages, options = prepare(cw_log_lines, hostname, tags)
+    sendLog(messages, options, key)
+
+def setup():
+    key = os.environ.get('LOGDNA_KEY', None)
+    hostname = os.environ.get('LOGDNA_HOSTNAME', None)
+    tags = os.environ.get('LOGDNA_TAGS', None)
+    return key, hostname, tags
+
+def decodeEvent(event):
+    cw_data = str(event['awslogs']['data'])
+    cw_logs = gzip.GzipFile(fileobj=StringIO(cw_data.decode('base64', 'strict'))).read()
+    return json.loads(cw_logs)
+
+def prepare(cw_log_lines, hostname=None, tags=None):
+    messages = list()
+    options = dict()
+    if 'logGroup' in cw_log_lines:
+        app = cw_log_lines['logGroup'].split('/')[-1]
+    if 'logStream' in cw_log_lines:
+        options['hostname'] = cw_log_lines['logStream']
+    if hostname is not None:
+        options['hostname'] = hostname
+    if tags is not None:
+        options['tags'] = tags
+    for cw_log_line in cw_log_lines['logEvents']:
+        message = {
+            'line': cw_log_line['message'],
+            'timestamp': cw_log_line['timestamp'],
+            'file': app}
+        messages.append(sanitizeMessage(message))
+    return messages, options
+
+def sanitizeMessage(message):
+    if message and message['line']:
+        if len(message['line']) > MAX_LINE_LENGTH:
+            message['line'] = message['line'][:MAX_LINE_LENGTH] + ' (cut off, too long...)'
+    return message
+
+def sendLog(messages, options, key=None):
+    if key is not None:
+        data = {'e': 'ls', 'ls': messages}
+        requests.post(
+            url=LOGDNA_URL,
+            json=data,
+            auth=('user', key),
+            params={
+                'hostname': options['hostname'],
+                'tags': options['tags'] if 'tags' in options else None},
+            stream=True,
+            timeout=MAX_REQUEST_TIMEOUT)
