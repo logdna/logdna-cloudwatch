b17e9f2f57a211667c6f3565fb1a44dfd60114f6
Samir Musali <samir.musali@logdna.com>
2019-03-24 13:06:24 -0400

    Updates

--- python/logdna_cloudwatch.py
+++ python/logdna_cloudwatch.py
@@ -37,27 +37,38 @@ def decodeEvent(event):
 
 # Preparing the Payload:
 def prepare(cw_log_lines, hostname=None, tags=None):
+    
     messages = list()
     options = dict()
     app = 'CloudWatch'
-    meta = {'type': app}
+    
+    meta = {
+        'type': app
+    }
+    
     if 'logGroup' in cw_log_lines:
         app = cw_log_lines['logGroup'].split('/')[-1]
         meta['group'] = cw_log_lines['logGroup'];
+    
     if 'logStream' in cw_log_lines:
         options['hostname'] = cw_log_lines['logStream'].split('/')[-1].split(']')[-1]
         meta['stream'] = cw_log_lines['logStream']
+    
     if hostname is not None:
         options['hostname'] = hostname
+    
     if tags is not None:
         options['tags'] = tags
+    
     for cw_log_line in cw_log_lines['logEvents']:
         message = {
             'line': cw_log_line['message'],
             'timestamp': cw_log_line['timestamp'],
             'file': app,
-            'meta': meta}
+            'meta': meta
+        }
         messages.append(sanitizeMessage(message))
+    
     return messages, options
 
 # Polishing the Message:
