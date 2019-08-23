346b21a3a60e74958b87c5d12a9b15734eb48dc2
vilyapilya <13.veter@gmail.com>
2019-08-23 10:57:35 -0700

    adds tests

---
+++ node.js/test/index.test.js
@@ -0,0 +1,7 @@
+const index = require ('../index.js')
+
+describe('#indexOf()', function() {
+   it('should return -1 when the value is not present', function(){
+     index.prepareLogs()
+   });
+});
