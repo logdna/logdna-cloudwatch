a8607760eaee542f5252d3721fd9150fbefd43cb
vilyapilya <13.veter@gmail.com>
2019-11-19 15:23:11 -0800

    refactor

--- logdna/index.js
+++ logdna/index.js
@@ -1,32 +1,18 @@
+// Internal Libraries
+const constants = require('./constants.js');
+const createLogs = require('./parsers/createLogs.js').createLogs;
+const pkg = require('./package.json');
+
 // External Libraries
 const agent = require('agentkeepalive');
 const asyncRetry = require('async').retry;
 const request = require('request');
-const zlib = require('zlib');
 
-// Constants
-const MAX_LINE_LENGTH = parseInt(process.env.LOGDNA_MAX_LINE_LENGTH) || 32000;
-const MAX_REQUEST_TIMEOUT_MS = parseInt(process.env.LOGDNA_MAX_REQUEST_TIMEOUT) || 30000;
-const FREE_SOCKET_TIMEOUT_MS = parseInt(process.env.LOGDNA_FREE_SOCKET_TIMEOUT) || 300000;
-const LOGDNA_URL = process.env.LOGDNA_URL || 'https://logs.logdna.com/logs/ingest';
-const MAX_REQUEST_RETRIES = parseInt(process.env.LOGDNA_MAX_REQUEST_RETRIES) || 5;
-const REQUEST_RETRY_INTERVAL_MS = parseInt(process.env.LOGDNA_REQUEST_RETRY_INTERVAL) || 100;
-const DEFAULT_HTTP_ERRORS = [
-    'ECONNRESET'
-    , 'EHOSTUNREACH'
-    , 'ETIMEDOUT'
-    , 'ESOCKETTIMEDOUT'
-    , 'ECONNREFUSED'
-    , 'ENOTFOUND'];
-
-const INTERNAL_SERVER_ERROR = 500;
 // Get Configuration from Environment Variables
 const getConfig = () => {
-    const pkg = require('./package.json');
     let config = {
         UserAgent: `${pkg.name}/${pkg.version}`
     };
-
     if (process.env.LOGDNA_KEY) config.key = process.env.LOGDNA_KEY;
     if (process.env.LOGDNA_HOSTNAME) config.hostname = process.env.LOGDNA_HOSTNAME;
     if (process.env.LOGDNA_TAGS && process.env.LOGDNA_TAGS.length > 0) {
@@ -34,37 +20,6 @@ const getConfig = () => {
     }
 
     return config;
-};
-
-// Parse the GZipped Log Data
-const parseEvent = (event) => {
-    return JSON.parse(zlib.unzipSync(Buffer.from(event.awslogs.data, 'base64')));
-};
-
-// Prepare the Messages and Options
-const prepareLogs = (eventData) => {
-    return eventData.logEvents.map((event) => {
-        return {
-            line: JSON.stringify({
-                message: event.message
-                , source: 'cloudwatch'
-                , event: {
-                    type: eventData.messageType
-                    , id: event.id
-                }
-                , log: {
-                    group: eventData.logGroup
-                    , stream: eventData.logStream
-                }
-            })
-            , timestamp: event.timestamp
-            , file: eventData.logStream
-            , meta: {
-                owner: eventData.owner
-                , filters: eventData.subscriptionFilters
-            }
-        };
-    });
 };
 
 // Ship the Logs
@@ -77,7 +32,7 @@ const sendLine = (payload, config, callback) => {
 
     // Prepare HTTP Request Options
     const options = {
-        url: LOGDNA_URL
+        url: constants.LOGDNA_URL
         , qs: config.tags ? {
             tags: config.tags
             , hostname: hostname
@@ -96,28 +51,28 @@ const sendLine = (payload, config, callback) => {
             'Content-Type': 'application/json; charset=UTF-8'
             , 'user-agent': config.UserAgent
         }
-        , timeout: MAX_REQUEST_TIMEOUT_MS
+        , timeout: constants.MAX_REQUEST_TIMEOUT_MS
         , withCredentials: false
         , agent: new agent.HttpsAgent({
-            freeSocketTimeout: FREE_SOCKET_TIMEOUT_MS
+            freeSocketTimeout: constants.FREE_SOCKET_TIMEOUT_MS
         })
     };
 
     // Flush the Log
     asyncRetry({
-        times: MAX_REQUEST_RETRIES
+        times: constants.MAX_REQUEST_RETRIES
         , interval: (retryCount) => {
-            return REQUEST_RETRY_INTERVAL_MS * Math.pow(2, retryCount);
+            return constants.REQUEST_RETRY_INTERVAL_MS * Math.pow(2, retryCount);
         }
         , errorFilter: (errCode) => {
-            return DEFAULT_HTTP_ERRORS.includes(errCode) || errCode === 'INTERNAL_SERVER_ERROR';
+            return constants.DEFAULT_HTTP_ERRORS.includes(errCode) || errCode === 'INTERNAL_SERVER_ERROR';
         }
     }, (reqCallback) => {
         return request(options, (error, response, body) => {
             if (error) {
                 return reqCallback(error.code);
             }
-            if (response.statusCode >= INTERNAL_SERVER_ERROR) {
+            if (response.statusCode >= constants.INTERNAL_SERVER_ERROR) {
                 return reqCallback('INTERNAL_SERVER_ERROR');
             }
             return reqCallback(null, body);
@@ -129,6 +84,6 @@ const sendLine = (payload, config, callback) => {
 };
 
 // Main Handler
-exports.handler = (event, context, callback) => {
-    return sendLine(prepareLogs(parseEvent(event)), getConfig(), callback);
+exports.handler = (event, callback) => {
+    return sendLine(createLogs(event), getConfig(), callback);
 };
---
+++ logdna/parsers/createLogs.js
@@ -0,0 +1,34 @@
+// External Libraries
+const zlib = require('zlib');
+
+// Parse the GZipped Log Data
+const parseEvents = (event) => {
+    return JSON.parse(zlib.unzipSync(Buffer.from(event.awslogs.data, 'base64')));
+};
+
+// Prepare the Messages and Options
+exports.createLogs = (events) => {
+    const parsedEvents = parseEvents(events);
+    return parsedEvents.logEvents.map((event) => {
+        return {
+            line: JSON.stringify({
+                message: event.message
+                , source: 'cloudwatch'
+                , event: {
+                    type: parsedEvents.messageType
+                    , id: event.id
+                }
+                , log: {
+                    group: parsedEvents.logGroup
+                    , stream: parsedEvents.logStream
+                }
+            })
+            , timestamp: event.timestamp
+            , file: parsedEvents.logStream
+            , meta: {
+                owner: parsedEvents.owner
+                , filters: parsedEvents.subscriptionFilters
+            }
+        };
+    });
+};
